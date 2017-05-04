ca5ed945d8fed8c88cb99f866c794d3725c393d2
imonkey <i0932702567@gmail.com>
2017-05-04 10:07:28 +0800

    init

---
+++ app/src/main/java/tw/imonkey/plc2go/DeviceRPI3IOActivity.java
@@ -0,0 +1,13 @@
+package tw.imonkey.plc2go;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceRPI3IOActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_rpi3_io);
+    }
+}
